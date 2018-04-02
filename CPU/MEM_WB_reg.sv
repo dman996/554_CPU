f3f3a2da2e47b864bfa7612c09b0b267a1326212
dman996 <d.man.996@gmail.com>
2018-04-02 18:59:55 -0500

    written with stall and flush signals but not tested

---
+++ CPU/MEM_WB_reg.sv
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////
+// Module Name: MEM_WB_reg                          //
+// Author: Dustin R. Wiczek                        //
+// Module Description: This module is for the     //
+//  pipeline register between MEM and WB stages  //
+//  and includes signals to flush and stall the //
+//  values that is holds.                      //
+////////////////////////////////////////////////
+module MEM_WB_reg(
+    input clk,
+    input rst_n,
+    input stall,
+    input flush,
+    // pipeline reg signals
+    input [31:0] alu_out_in,
+    input [3:0] reg_dst_in,
+    output [31:0] alu_out_out,
+    output [3:0] reg_dst_out,
+    //control signals
+    input reg_wr_in,
+    input wb_sel_in,
+    output reg_wr_out,
+    output wb_sel_out
+);
+
+always @(posedge clk, negedge rst_n) begin
+    if(!rst_n) begin
+        alu_out_out = 0;
+        reg_dst_out = 0;
+        reg_wr_out = 0;
+        wb_sel_out = 0;
+    end
+    else if(flush) begin
+        alu_out_out = 0;
+        reg_dst_out = 0;
+        reg_wr_out = 0;
+        wb_sel_out = 0;
+    end
+    else if(stall) begin
+        alu_out_out = alu_out_out;
+        reg_dst_out = reg_dst_out;
+        reg_wr_out = reg_wr_out;
+        wb_sel_out = wb_sel_out;
+    end
+    else begin
+        alu_out_out = alu_out_in;
+        reg_dst_out = reg_dst_in;
+        reg_wr_out = reg_wr_in;
+        wb_sel_out = wb_sel_in;
+    end
+end
+
+endmodule;
+// line for revision control version 1.0
